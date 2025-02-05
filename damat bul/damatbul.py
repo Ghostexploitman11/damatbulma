b3961604504f9be1f40d147fdba56fc871588e36
Ghostexploitman11 <hayalet001@proton.me>
2025-02-05 15:54:04 -0500

    Add files via upload

---
+++ damat bul/damatbul.py
@@ -0,0 +1,29 @@
+import random
+import time
+import sys
+
+
+isim = input("İsminizi giriniz: ")
+
+
+dosya_yolu = "/root/Desktop/damat bul/isimler.txt"
+
+
+with open(dosya_yolu, 'r', encoding='utf-8') as file:
+    isimler = file.readlines()
+
+
+isimler = [isim.strip() for isim in isimler]
+
+
+rastgele_isim = random.choice(isimler)
+
+print(isim + " " + "hanım için damat aranıyor", end="", flush=True)
+
+
+for _ in range(30):
+    print(".", end="", flush=True)
+    time.sleep(1)  # 1 saniye bekle
+
+
+print("\n" + isim + " " + "hanım için damat bulundu: " + rastgele_isim)
